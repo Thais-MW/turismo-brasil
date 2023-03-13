4df7507a513f30bc08f593278769553962984f76
Thais-MW <thais.wermelinger@aluno.cefet-rj.br>
2023-03-12 23:55:03 -0300

    Adiciona requisicao a API de moedas

---
+++ scripts/conversao.js
@@ -0,0 +1,44 @@
+const API_BASE = "https://api.apilayer.com/fixer";
+const fetchBase = async (url) =>
+  await fetch(`${API_BASE}/${url}`, {
+    headers: { apikey: "RABusSzWZhYGY9vrv4naDlqBgNnngLdz" },
+  });
+
+export async function converter(moedaOrigem, valor) {
+  const response = await fetchBase(
+    `convert?to=BRL&from=${moedaOrigem}&amount=${valor}`
+  );
+
+  const result = await response.json();
+
+  return result.result;
+}
+
+export async function exibirDisponiveis() {
+  const response = await fetchBase("symbols");
+
+  const result = await response.json();
+
+  const select = document.querySelector("#moedasDisponiveis");
+
+  // console.log(result.symbols);
+  select.options.length = 0;
+  select.options[select.options.length] = new Option("Escolha a moeda", "");
+
+  for (const moeda in result.symbols) {
+    select.options[select.options.length] = new Option(
+      result.symbols[moeda],
+      moeda
+    );
+  }
+}
+
+export async function historico(moedaOrigem) {
+  const response = await fetchBase(
+    `timeseries?start_date=2023-03-01&end_date=2023-03-12&base=${moedaOrigem}&symbols=BRL`
+  );
+
+  const result = await response.json();
+
+  return result.rates;
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+import { converter, exibirDisponiveis, historico } from "./conversao.js";
+
 // adiciona partials ao código
 $(function () {
   var includes = $("[data-include]");
@@ -6,3 +8,54 @@ $(function () {
     $(this).load(file);
   });
 });
+
+exibirDisponiveis();
+
+document
+  .querySelector("#botaoConversao")
+  .addEventListener("click", async () => {
+    const valor = +document.querySelector("#valorMoeda").value;
+    const select = document.querySelector("#moedasDisponiveis");
+    const moeda = select.options[select.selectedIndex].value;
+    const valorConvertido = await converter(moeda, valor);
+    const nf = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+
+    document.querySelector("#resultado").innerText = nf.format(valorConvertido);
+    const variacao = await historico(moeda);
+    const diasVariacao = [];
+    const variacaoEmReais = [];
+
+    for (const data in variacao) {
+      diasVariacao.push(
+        `${new Date(data).getDate()}/${new Date(data).getMonth() + 1}`
+      );
+      variacaoEmReais.push(+variacao[data].BRL.toFixed(2));
+    }
+
+    console.log(diasVariacao);
+    console.log(variacaoEmReais);
+    const contexto = document.querySelector("#grafico");
+    new Chart(contexto, {
+      type: "line",
+      data: {
+        labels: diasVariacao,
+        datasets: [
+          {
+            label: "Variação em dias",
+            data: variacaoEmReais,
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: false,
+          },
+        },
+      },
+    });
+  });
